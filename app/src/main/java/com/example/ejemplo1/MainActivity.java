2531218a188a8f28f6a5556a6f5ba170f90298e5
jose <jlicancura@ing.ucsc.cl>
2021-05-16 23:39:27 -0400

    xcv

--- app/src/main/java/com/example/ejemplo1/MainActivity.java
+++ app/src/main/java/com/example/ejemplo1/MainActivity.java
@@ -19,9 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText edit1, edit2;
     private Button btn1, btnRegistar;
     private String valor;
-
+    private String contadors;
     private SharedPreferences preferences;
-
+    int c=0;
+    private TextView textocontador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +34,33 @@ public class MainActivity extends AppCompatActivity {
         btn1 = findViewById(R.id.btn1);
         edit2 = findViewById(R.id.edit2);
         btnRegistar = findViewById(R.id.btnRegistar);
+        textocontador= findViewById(R.id.contador);
 
         preferences = getSharedPreferences("Preference", Context.MODE_PRIVATE);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
                 String textoEdit1 = edit1.getText().toString();
+                String textotextocontador=textocontador.getText().toString();
                 text1.setText(textoEdit1);
+               // textocontador.setText(textotextocontador);
                 if (isValidForm()) {
                     Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_LONG).show();
                     saveShared(textoEdit1);
 
+                    int aux=c+1;
+                    contadors=String.valueOf(aux);
+                    saveShared2(contadors);
+                    textocontador.setText(contadors);
+                    c=aux;
                     sendSecondActivity(textoEdit1);
+
+
+
+
                 } else {
                     Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
                 }
@@ -72,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         text1.setText( "Nombre guardado: "+getSharedNombre());
-
+        textocontador.setText("numero de intentos "+getSharedcontador());
     }
 
     private boolean isValidForm() {
@@ -105,12 +119,23 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString( "nombre",nombre);
         editor.apply();
-    }
 
+    }
+    private void saveShared2(String contador){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString( "contador",contador);
+        editor.apply();
+
+    }
+    private void funcont(){
+
+    }
     private String getSharedNombre(){
         return preferences.getString( "nombre","");
     }
-
+    private String getSharedcontador(){
+        return preferences.getString( "contador","");
+    }
 }
 
 
